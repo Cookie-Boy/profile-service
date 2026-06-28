bfa09a4d471f820cea282ac1fb0991dc9cb87d4c
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-06-28 16:34:36 +0700

    Update pet-info template

--- src/main/java/ru/sibsutis/profile/api/controller/PetQrViewController.java
+++ src/main/java/ru/sibsutis/profile/api/controller/PetQrViewController.java
@@ -6,7 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.sibsutis.profile.api.dto.OwnerResponse;
 import ru.sibsutis.profile.api.dto.PetResponse;
+import ru.sibsutis.profile.core.service.OwnerService;
 import ru.sibsutis.profile.core.service.PetService;
 
 @Controller
@@ -15,11 +17,16 @@ import ru.sibsutis.profile.core.service.PetService;
 public class PetQrViewController {
 
     private final PetService petService;
+    private final OwnerService ownerService;
 
     @GetMapping("/qr-scan/{qrCode}")
     public String viewPetByQrCode(@PathVariable String qrCode, Model model) {
         PetResponse pet = petService.getPetByQrCode(qrCode);
+        OwnerResponse owner = ownerService.getOwnerById(pet.getOwnerId());
+
         model.addAttribute("pet", pet);
+        model.addAttribute("owner", owner);
+
         return "pet-info";
     }
 }
